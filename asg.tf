3b6b28c452f3d897d7d2d47ede2419c699a59b4c
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-05-14 23:48:30 +0530

    test for frontend

--- asg.tf
+++ asg.tf
@@ -9,3 +9,14 @@ module "asg" {
   HEALTH          = "/"
   APP_ARTIFACT_VERSION = var.APP_ARTIFACT_VERSION
 } // calling other module(terraform-asg) as asg module also pass the variables to it.
+
+
+//resource "aws_lb_listener" "frontend" {
+//  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_LISTENER_ARN
+//  port              = "80"
+//  protocol          = "HTTP"
+//  default_action {
+//    type             = "forward"
+//    target_group_arn = module.asg.TG_ARN
+//  }
+//}
