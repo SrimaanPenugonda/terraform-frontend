6345426df7b604333d2eef6c32470a87c6fe4cf3
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-05-15 11:03:01 +0530

    test

--- asg.tf
+++ asg.tf
@@ -11,12 +11,12 @@ module "asg" {
 } // calling other module(terraform-asg) as asg module also pass the variables to it.
 
 
-//resource "aws_lb_listener" "frontend" {
-//  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_LISTENER_ARN
-//  port              = "80"
-//  protocol          = "HTTP"
-//  default_action {
-//    type             = "forward"
-//    target_group_arn = module.asg.TG_ARN
-//  }
-//}
+resource "aws_lb_listener" "frontend" {
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_LISTENER_ARN
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = module.asg.TG_ARN
+  }
+}
